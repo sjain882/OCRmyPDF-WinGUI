6c621e93142e365402a0fd5c18a8c8450deab53c
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-25 22:09:08 +0000

    Add Utilities class with StringBuilder extension method

---
+++ OCRmyPDF-WinGUI/Logic/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrMyPdf.Logic
+{
+    public static class Utilities
+    {
+        public static StringBuilder AppendWithSeparator(StringBuilder sb,
+                                                        char separator,
+                                                        string valueToAppend)
+        {
+            // If this is the first string appended, don't add the separator
+            if (sb.Length > 0)
+                sb.Append(separator);
+
+            sb.Append(valueToAppend);
+            return sb;
+        }
+    }
+}
