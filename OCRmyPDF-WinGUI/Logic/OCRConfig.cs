ab6822356bb9db883bb4dbbe6902e88ec3bf1ddd
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-25 21:26:13 +0000

    Remove OCRConfigTypes.cs

--- OCRmyPDF-WinGUI/Logic/OCRConfig.cs
+++ OCRmyPDF-WinGUI/Logic/OCRConfig.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace OcrMyPdf.Logic
 {
+    enum ProcessingPolicy
+    {
+        Default = 0,
+        SkipText = 1,
+        RedoOCR = 2,
+        ForceOCR = 3
+    }
+
     internal class OCRConfig
     {
         public ProcessingPolicy processingPolicy { get; set; }
