89942b5fde8840d9ccba2d0714675424c88890e7
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-25 21:37:13 +0000

    Complete OCRConfig.cs

--- OCRmyPDF-WinGUI/Logic/OCRConfig.cs
+++ OCRmyPDF-WinGUI/Logic/OCRConfig.cs
@@ -8,15 +8,37 @@ namespace OcrMyPdf.Logic
 {
     enum ProcessingPolicy
     {
-        Default = 0,
-        SkipText = 1,
-        RedoOCR = 2,
-        ForceOCR = 3
+        Default     = 0,        // Default in CLI + GUI
+        SkipText    = 1,
+        RedoOCR     = 2,
+        ForceOCR    = 3
+    }
+
+    enum PDFType
+    {
+        PDF         = 0,        // Make this default in GUI
+        PDFA        = 1         // Defualt in CLI
+    }
+
+    enum OptimisationLevel
+    {
+        Disabled    = 0,
+        Lossless    = 1,        // Default in CLI + GUI
+        Lossy       = 2,
+        AggrLossy   = 3
     }
 
     internal class OCRConfig
     {
         public ProcessingPolicy processingPolicy { get; set; }
+
+        public bool rotate { get; set; }               // Make default T in GUI
+
+        public bool deskew { get; set; }               // Make default T in GUI
+
+        public string newTitle { get; set; }
+
+        public string outputSuffix { get; set; }       // Default "_Searchable"
 
     }
 }
