feab6747c8ad5dac692fc038c33fe20134f6ecef
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-25 21:21:36 +0000

    Add separate file for ocrmypdf params, corresponding to OCRConfigTypes

---
+++ OCRmyPDF-WinGUI/Logic/OCRConfigParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrMyPdf.Logic
+{
+    static class OcrParams
+    {
+        public static string[] ProcessingPolicy =
+        {
+        "",
+        "--skip-text",
+        "--redo-ocr",
+        "--force-ocr"
+        };
+    }
+}
