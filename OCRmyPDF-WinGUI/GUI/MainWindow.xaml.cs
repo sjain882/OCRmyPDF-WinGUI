76fba050cafc85328e37df50faed950f37f53524
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-26 20:15:10 +0000

    Rename GUI namespace

--- OCRmyPDF-WinGUI/GUI/MainWindow.xaml.cs
+++ OCRmyPDF-WinGUI/GUI/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OcrMyPdf
+namespace OcrMyPdf.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
