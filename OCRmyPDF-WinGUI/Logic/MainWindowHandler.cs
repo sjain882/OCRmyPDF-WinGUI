ed52b91b8fb59714e7062f2c961f4e2c376a03ca
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-27 20:56:29 +0000

    Implement first data binding (test)

--- OCRmyPDF-WinGUI/Logic/MainWindowHandler.cs
+++ OCRmyPDF-WinGUI/Logic/MainWindowHandler.cs
@@ -12,10 +12,35 @@ namespace OcrMyPdf.Logic
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        public OCRConfig ocrConfig;
+
+        public List<string> inputFilePaths;
+
+        // OCRConfig
+        public bool Rotate
+        {
+            get { return ocrConfig.rotate; }
+            set
+            {
+                ocrConfig.rotate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowHandler()
+        {
+            ocrConfig = new OCRConfig();
+            inputFilePaths = new List<string>();
+        }
+
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+
+
+
 
     }
 
