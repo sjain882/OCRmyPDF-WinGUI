e37ab07d72bb856b7f11f05a08b5f599e89a2a0f
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-27 21:05:52 +0000

    Create OCRConfigNames.cs for options UI friendlynames

---
+++ OCRmyPDF-WinGUI/Logic/OCRConfigNames.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrMyPdf.Logic
+{
+    public static class OCRConfigNames
+    {
+
+        public static string[] ProcessingPolicy =
+        {
+        "Default: Stop if text already present",
+        "Redo: Analyse &amp; redo text where applicable",
+        "Force: Wipe &amp; redo all text from scratch"
+        };
+
+        public static string[] PDFType =
+        {
+        "PDF",
+        "PDF/A"
+        };
+
+        public static string[] OptimisationLevel =
+        {
+        "Default (recommended)",
+        "Lossless",
+        "Lossy",
+        "Aggressive Lossy",
+        "Disabled (not recommended)"
+        };
+
+        public static string rotate = "Rotate";
+
+        public static string deskew = "Deskew";
+
+    }
+}
