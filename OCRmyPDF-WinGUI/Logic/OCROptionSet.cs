c09156050a02038af6eefa33e79746ad6f86fabb
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-11-30 21:24:29 +0000

    Create OCROptionSet.cs to replace OCRConfig

---
+++ OCRmyPDF-WinGUI/Logic/OCROptionSet.cs
@@ -0,0 +1,25 @@
+﻿using OcrMyPdf.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrMyPdf.Logic
+{
+    public class OCROptionSet
+    {
+        public MultiOptionTemplate processingPolicy { get; set; } = ProcessingPolicy.Default;
+
+        public MultiOptionTemplate pdfType { get; set; } = PDFType.PDF;
+
+        public MultiOptionTemplate optimisationLevel { get; set; } = OptimisationLevel.Default;
+
+        public string outputSuffix { get; set; } = "_Searchable";
+
+        public bool rotate { get; set; } = true;
+
+        public bool deskew { get; set; } = true;
+
+    }
+}
