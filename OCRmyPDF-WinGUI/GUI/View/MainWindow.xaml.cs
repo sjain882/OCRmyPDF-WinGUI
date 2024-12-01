750c79fdcadef12b8ce80be75f7e07ab08ee585b
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-12-01 16:56:11 +0000

    Implement Start button click function

--- OCRmyPDF-WinGUI/GUI/View/MainWindow.xaml.cs
+++ OCRmyPDF-WinGUI/GUI/View/MainWindow.xaml.cs
@@ -59,7 +59,7 @@ namespace OcrMyPdf.Gui.View
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            OCRRunner.RunOCR(winHandler.filePathsList.ToArray(), winHandler.ocrOptions);
         }
     }
 }
