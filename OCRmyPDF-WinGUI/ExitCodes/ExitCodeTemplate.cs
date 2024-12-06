568cae4eb780ab7b7ae710c9dcc96877819ade73
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-12-06 22:41:05 +0000

    Add ocrmypdf.exe error / exit code definition classes

---
+++ OCRmyPDF-WinGUI/ExitCodes/ExitCodeTemplate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrMyPdf.ExitCodes
+{
+    public class ExitCodeTemplate
+    {
+        public int code {  get; set; }
+
+        public string displayName { get; set; }
+
+        public string description { get; set; }
+
+        public string nameAndDesc
+        {
+            get
+            {
+                return this.displayName + " - " + this.description;
+            }
+        }
+
+        public ExitCodeTemplate(int code, string displayName, string description)
+        {
+            this.code = code;
+            this.displayName = displayName;
+            this.description = description;
+        }
+
+        public ExitCodeTemplate(ExitCodeTemplate errorCodeTemplate)
+        {
+            this.code = errorCodeTemplate.code;
+            this.displayName = errorCodeTemplate.displayName;
+            this.description = errorCodeTemplate.description;
+        }
+    }
+}
