2bee245e789ca22f308183fb9da66ece5efc15fa
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-12-21 11:33:42 +0000

    Create OCRError class to better store errors (#3)

---
+++ OCRmyPDF-WinGUI/Logic/OCRError.cs
@@ -0,0 +1,16 @@
+﻿using OcrMyPdf.Logic.ExitCodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrMyPdf.Logic
+{
+    public class OCRError
+    {
+        public string filePath { get; set; }
+
+        public ExitCodeTemplate exitCode { get; set; }
+    }
+}
