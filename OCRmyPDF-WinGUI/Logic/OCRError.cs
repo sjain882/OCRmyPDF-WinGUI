0b7a13b75794dcba81c595fac52b34074ed0d768
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-12-21 20:05:54 +0000

    Add missing constructor to OCRError (#3)

--- OCRmyPDF-WinGUI/Logic/OCRError.cs
+++ OCRmyPDF-WinGUI/Logic/OCRError.cs
@@ -12,5 +12,11 @@ namespace OcrMyPdf.Logic
         public string filePath { get; set; }
 
         public ExitCodeTemplate exitCode { get; set; }
+
+        public OCRError(string filePath, ExitCodeTemplate exitCode)
+        {
+            this.filePath = filePath;
+            this.exitCode = exitCode;
+        }
     }
 }
