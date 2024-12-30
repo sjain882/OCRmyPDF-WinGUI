6565134f32f44c0fb5266032b2c96990cb2aed24
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-12-30 00:58:13 +0000

    Add version information

--- OCRmyPDF-WinGUI/AssemblyInfo.cs
+++ OCRmyPDF-WinGUI/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +9,15 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
+
+[assembly: AssemblyTitle("Simple frontend for OCRmyPDF.")]
+[assembly: AssemblyDescription("Simple frontend for OCRmyPDF.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sjain882")]
+[assembly: AssemblyProduct("OCRmyPDF-WinGUI")]
+[assembly: AssemblyCopyright("Copyright ©  sjain882 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
