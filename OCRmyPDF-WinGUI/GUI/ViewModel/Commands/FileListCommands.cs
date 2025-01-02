5bc60ae50ba1f7595b63ebf4003ec1a22b563d37
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-01-02 17:38:41 +0000

    Cleanup FileListCommands.cs

--- OCRmyPDF-WinGUI/GUI/ViewModel/Commands/FileListCommands.cs
+++ OCRmyPDF-WinGUI/GUI/ViewModel/Commands/FileListCommands.cs
@@ -10,21 +10,23 @@ namespace OcrMyPdf.Gui.ViewModel.Commands
 
         public event EventHandler CanExecuteChanged;
 
+
         public RemoveFileCommand(MainWindowViewModel viewModel)
         {
             ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
         }
+
 
         public bool CanExecute(object parameter)
         {
             return true;
         }
 
+
         // Called when Start button pressed
         public void Execute(object parameter)
         {
             ViewModel.filePathsList.Remove(ViewModel.FilePathsList_Selected);
-            // MessageBox.Show("hi");
         }
     }
 }
