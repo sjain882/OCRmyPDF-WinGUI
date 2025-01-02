7e6b25b3e5912ead340fe8000bf34da36ebf149e
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-01-02 17:44:33 +0000

    Make defaults more obvious in OCROptionSet.cs

--- OCRmyPDF-WinGUI/Logic/OCROptionSet.cs
+++ OCRmyPDF-WinGUI/Logic/OCROptionSet.cs
@@ -4,21 +4,22 @@ namespace OcrMyPdf.Logic
 {
     public class OCROptionSet
     {
-        public MultiOptionTemplate processingPolicy { get; set; } = ProcessingPolicy.OptionList.Single(o => o.identifier == "RedoOCR");
+        // Option                                                   // Default value
+        public MultiOptionTemplate processingPolicy { get; set; }   = ProcessingPolicy.OptionList.Single(o => o.identifier == "RedoOCR");
 
-        public MultiOptionTemplate pdfType { get; set; } = PDFType.OptionList.Single(o => o.identifier == "PDF");
+        public MultiOptionTemplate pdfType { get; set; }            = PDFType.OptionList.Single(o => o.identifier == "PDF");
 
-        public MultiOptionTemplate optimisationLevel { get; set; } = OptimisationLevel.OptionList.Single(o => o.identifier == "Default");
+        public MultiOptionTemplate optimisationLevel { get; set; }  = OptimisationLevel.OptionList.Single(o => o.identifier == "Default");
 
-        public string outputSuffix { get; set; } = "_Searchable";
+        public string outputSuffix { get; set; }                    = "_Searchable";
 
-        public bool rotate { get; set; } = true;
+        public bool rotate { get; set; }                            = true;
 
-        public bool deskew { get; set; } = false;
+        public bool deskew { get; set; }                            = false;
 
-        public bool clearSuccesses { get; set; } = false;
+        public bool clearSuccesses { get; set; }                    = false;
 
-        public bool separateDir { get; set; } = false;
+        public bool separateDir { get; set; }                       = false;
 
     }
 }
